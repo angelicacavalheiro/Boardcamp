9ab91cd82e7227477f717dd76ac061945d6f7842
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-10-15 16:26:54 -0300

    validações com joi e regex adicionadas

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from "cors"
 import pg from 'pg';
+import joi from 'joi';
 
 const { Pool } = pg;
 
@@ -15,6 +16,20 @@ const connection = new Pool({
 const app = express();
 app.use(express.json());
 
+const customersSchema = joi.object({
+  name: joi.string().min(1).max(30).required(),
+  phone: joi.string().pattern(/^[0-9]+$/).min(10).max(11).required(),
+  cpf: joi.string().pattern(/^[0-9]+$/).min(11).max(11).required(),
+  birthday: joi.string().pattern(/^[0-9]{4}\-[0-9]{2}\-[0-9]{1,2}$/).required(),
+})
+
+const gamesSchema = joi.object({
+  name: joi.string().min(1).max(30).required(),
+  image: joi.string().uri().required(),
+  stockTotal: joi.number().min(1).required(),
+  categoryId: joi.number().min(1).required(),
+  pricePerDay: joi.number().min(1).required(),
+})
 
 //-----------------------***************----------------------CATEGORIES--------***********************************-----------------//
 app.get('/categories', (req, res) => {
@@ -27,8 +42,7 @@ app.post('/categories', async (req, res) => {
     const { name } = req.body;
 
     if (!name || name.length === 0 ){
-        res.status(400).send('O nome não pode ser vazio!');
-        return
+        return res.status(400).send('O nome não pode ser vazio!');
     }
 
     //com isso a gente consegue jogar toda a logica do erro pra uma zona segura, 
@@ -54,73 +68,104 @@ app.post('/categories', async (req, res) => {
     } else {
         await connection.query('INSERT INTO categories (name) VALUES ($1);', [name]).then (result => {
             res.sendStatus(201);
-        })}      
-  });
+        }
+    )}      
+});
 
 //-----------------------***************---------------------JOGOS---------***********************************-----------------//
 
-  app.get('/games', async (req, res) => {
+app.get('/games', async (req, res) => {
 
-    let name = req.query.name;
+  let name = req.query.name;
 
-    if (!name || name.length === 0){   
-        connection.query('SELECT * FROM games').then(games => {
-            res.send(games.rows);
-          });            
-        
-    } else {
-        const result = await connection.query('SELECT * FROM games WHERE name ilike($1)', [`${name}%`])
-          if (result.rows.length){
-            res.send(result.rows);
-          } else {
-            res.send(`Não existe jogo começando com: ${name}`)
-          }  
-    }    
-
-  });
-  
-  app.post('/games', async (req, res) => {
-      const { name, image, stockTotal, categoryId, pricePerDay } = req.body;      
-
-      //validando existencia de nome e alguns valores
-      if (!name || name.length === 0 || stockTotal <= 0 || pricePerDay <= 0 ){
-        res.sendStatus(400);
-        return
-      }
-
-      //validando se nome é novo ou se ja existe
-      const resultName = await connection.query('SELECT name FROM games WHERE name = $1;', [name])
-      if(resultName.rows.length){
-        res.status(409).send("Esse nome ja existe")  
-      } else {
-
-      //validando id
-      const resultId = await connection.query('SELECT id FROM categories WHERE id = $1;', [categoryId])
-      if(resultId.rows.length){
-        connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);', [name, image, stockTotal, categoryId, pricePerDay]).then (resultName => {
-          res.sendStatus(201);
-        })
-      } else {
-        res.status(409).send("O id pra essa categoria ainda não existe, ou esta errado")
-      }}});
-
-   
-          
+  if (!name || name.length === 0){   
+      connection.query('SELECT * FROM games').then(games => {
+          res.send(games.rows);
+        });            
       
+  } else {
+      const result = await connection.query('SELECT * FROM games WHERE name ilike($1)', [`${name}%`])
+        if (result.rows.length){
+          res.send(result.rows);
+        } else {
+          res.send(`Não existe jogo começando com: ${name}`)
+        }  
+  }    
+
+});
+  
+app.post('/games', async (req, res) => {
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;      
+
+  const gamesData = { name, image, stockTotal, categoryId, pricePerDay  }     
+
+  const { error, value } = gamesSchema.validate(gamesData)
+  if (error){
+    console.log(error)
+    return res.sendStatus(400);
+  }
+
+  //validando se nome é novo ou se ja existe
+  const resultName = await connection.query('SELECT name FROM games WHERE name = $1;', [name])
+  if(resultName.rows.length){
+    res.status(409).send("Esse nome ja existe")  
+  } else {
+
+  //validando id
+  const resultId = await connection.query('SELECT id FROM categories WHERE id = $1;', [categoryId])
+    if(resultId.rows.length){
+      connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);', [name, image, stockTotal, categoryId, pricePerDay]).then (resultName => {
+        res.sendStatus(201);
+      })
+    } else {
+      res.status(400).send("O id pra essa categoria ainda não existe, ou esta errado")
+    }}
+});
+
+//-----------------------***************---------------------clientes---------***********************************-----------------//
+
+app.get('/customers', async (req, res) => {
+
+  let cpf = req.query.cpf;
+
+  if (!cpf || cpf.length === 0){   
+    connection.query('SELECT * FROM customers').then(customers => {
+      return res.send(customers.rows);
+    });
+  } 
+
+  const result = await connection.query('SELECT * FROM customers WHERE cpf ilike($1)', [`${cpf}%`])
+    if (result.rows.length){
+      res.send(result.rows);
+    } else {
+      res.send(`Não existem usuários começando com o CPF: ${cpf}`)
+    }  
+});
+
+app.post('/customers', async (req, res) => {
+  const { name, phone, cpf, birthday } = req.body;  
+  const customerData = { name, phone, cpf, birthday }     
+
+  const { error, value } = customersSchema.validate(customerData)
+  if (error){
+    console.log(error)
+    return res.sendStatus(400);
+  }
+
+  const resultCPF = await connection.query('SELECT cpf FROM customers WHERE cpf = $1;', [cpf])
+      if(resultCPF.rows.length){
+        res.status(409).send("Não foi possível cadastrar, esse CPF já esta cadastrado")  
+      } else {
+        connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);', [name, phone, cpf, birthday]).then(customers => {
+          res.sendStatus(201);
+      }
+  )};  
+})
+
+//-----------------------***************---------------------porta---------***********************************-----------------//
 
 app.listen(4000, () => {
   console.log('Server listening on port 4000.');
 });
 
-//-----------------------***************---------------------clientes---------***********************************-----------------//
-
-app.get('/customers', (req, res) => {
-    connection.query('SELECT * FROM customers').then(customers => {
-      res.send(customers.rows);
-    });
-  });
    
-
-app.listen(4001, () => {
-  console.log('Server listening on port 4001.');
-});
